fa9c374014d479650a8a1a0625bdb9f2f6d0ae5b
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-15 19:14:58 +0200

    Add description to solution of the exercise porownywanie.py

--- porownywanie.py
+++ porownywanie.py
@@ -1,3 +1,17 @@
+"""Opis
+Mając dane dwie liczby całkowite, określ relację między nimi, tzn. czy pierwsza jest mniejsza, większa, czy też równa drugiej.
+Źródło: https://onlinejudge.org/external/111/11172.pdf
+Specyfikacja
+Dane
+ - liczby całkowite z przedziału
+Wynik
+Znak relacji pomiędzy liczbami  i  , tzn. jeden ze znaków: , ,
+Przykład
+Dane
+a := 10
+b := 15
+Wynik
+<"""
 a = input().split()
 a = int(a[-1])
 print("to jest a",a)
